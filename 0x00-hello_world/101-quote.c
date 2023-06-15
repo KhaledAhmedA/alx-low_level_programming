faac2567aad825c8a2cad3bff515547d506f60c3
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-15 16:11:30 -0700

    print string without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * print string without using printf or puts
+ * Return: always 1 (Failure)
+ */
+int main(void)
+{
+	char varS[] = "and that piece of art is usful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, varS, 59);
+	return (1);
+}
