9f90a085286e899a4185c80d7f1515e1c2fe7928
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-15 14:57:49 -0700

    using puts function to print exact string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * print exact string
+ * Return alwyes 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
