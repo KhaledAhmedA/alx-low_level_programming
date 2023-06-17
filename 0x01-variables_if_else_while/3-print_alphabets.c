e99fdc2f4f8a9c9545e172b01da1732e72980bbd
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-16 18:07:12 -0700

    upper and lower alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print alphabets lower then upper with putchar
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char lalpha = 'a';
+	char ualpha = 'A';
+
+	while (lalpha <= 'z')
+	{
+		putchar(lalpha);
+		lalpha++;
+	}
+
+	while (ualpha < 'Z')
+	{
+		putchar(ualpha);
+		ualpha++;
+	}
+	putchar('\n');
+	return (0);
+}	
