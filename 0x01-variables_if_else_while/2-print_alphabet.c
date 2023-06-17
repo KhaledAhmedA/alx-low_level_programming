e1de9808a48f0ac5432f9a641fe1a5330cf55215
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-16 17:52:38 -0700

    alphabet lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print lowercase alphabet
+ * Return: Always 0 (Sucess)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+		putchar('\n');
+	}
+	return (0);
+}
