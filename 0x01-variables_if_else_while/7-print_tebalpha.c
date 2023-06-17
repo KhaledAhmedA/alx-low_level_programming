1beaae45b9852485ad8c76ee96eb41a1ec0221c5
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-16 19:06:18 -0700

    reversed alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print alphabets reversed by pucher
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
