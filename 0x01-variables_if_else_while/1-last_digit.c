b50811c21f113d135c7633366b130542dc711865
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-16 17:26:38 -0700

    last num and conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,22 +4,24 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * function to return kind of num if neg or pos or zero
- * Return: always 0 (Sucess)
-*/
+ * last num and conditions
+ * Return: Always 0 (Sucess)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
-        int n,ln;
+	int n, ln;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
 	ln = n % 10;
-	if (n > 5)
-		printf("Last digit of %d is %d and greater than 5\n", n, ln);
-	else if (n < 6)
-		printf("Last digit of %d is %d and less than 6 and not 0\n", n, ln);
-	else
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (ln > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, ln);
+	else if (ln == 0)
 		printf("Last digit of %d is %d and is 0\n", n, ln);
+	else
+		printif("Last digit of %d is %d and is less than 6 and not 0\n", n ln);
+	return (0);
 }
