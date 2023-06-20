da97f9567aea5999cd86da15993614cfe7baeab8
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-20 13:53:56 -0700

    acoording betty lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet to print lowercase alphabet
- * 
+ * print_alphabet - to print lowercase alphabet
  * Return: Always 0 (Sucess)
 */
-void print_alphabet(void){
+void print_alphabet(void)
+{
 	int alpha;
-	
-	for (alpha='a'; alpha <= 'z'; alpha++)
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		_putchar(alpha);
 	_putchar('\n');
 }
