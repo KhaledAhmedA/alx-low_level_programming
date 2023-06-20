a73fdfcb8ff7d288b81c059df4c1328686bd984e
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-20 14:21:55 -0700

    editing alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * print_alphabet_x10: that print alphabet 10 times
+ * print_alphabet_x10 - that print alphabet 10 times
  * Return: Always 0 (Sucess)
 */
 void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 1; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 97; j <= 122; j++)
 			_putchar(j);
