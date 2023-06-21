ac14e13e20141805540e5a099582d5f1b50982c7
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-20 20:09:32 -0700

    fabi

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 	num1 = 0;
 	num2 = 1;
 	log1 = log2 = 1;
-	for (i = 1; i < 98; i++)
+	for (i = 1; i <= 98; i++)
 	{
 		if (log1)
 		{
@@ -24,20 +24,20 @@ int main(void)
 		{
 			if (log2)
 			{
-				ln1 = num1 % 10000000000;
-				ln2 = num2 % 10000000000;
-				num1 = num1 / 10000000000;
-				num2 = num2 / 10000000000;
+				ln1 = num1 % 1000000000;
+				ln2 = num2 % 1000000000;
+				num1 = num1 / 1000000000;
+				num2 = num2 / 1000000000;
 				log2 = 0;
 			}
 			box2 = (ln1 + ln2);
-			box1 = num1 + num2 + (box2 / 10000000000);
+			box1 = num1 + num2 + (box2 / 1000000000);
 			printf(", %ld", box1);
-			printf("%ld", box2 % 10000000000);
+			printf("%ld", box2 % 1000000000);
 			num1 = num2;
 			ln1 = ln2;
 			num2 = box1;
-			ln2 = (box2 % 10000000000);
+			ln2 = (box2 % 1000000000);
 		}
 		if (((num1 + num2) < 0) && log1 == 1)
 			log1 = 0;
