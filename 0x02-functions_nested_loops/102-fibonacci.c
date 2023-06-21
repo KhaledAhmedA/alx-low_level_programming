b39b26f273dc80f56e6b60b32cc78f10c8222519
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-20 18:59:23 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	int dist;
+	long int num1, num2, box;
+
+	num1 = 1;
+	num2 = 2;
+
+	for (dist = 0; dist < 50; dist++)
+	{
+		box = num1 + num2;
+		printf("%ld", box);
+
+		num1 = num2;
+		num2 = box;
+
+		if (dist == 49)
+			printf("\n");
+		else
+			printf("\n");
+	}
+	return (0);
+}
