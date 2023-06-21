c2fec46bf75239d32b843d3038c5fa74a3e0238a
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-20 17:43:12 -0700

    sum numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function sum two numbers
+ * @x: first number
+ * @y: second number
+ * Return: Always 0 (Sucess)
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
