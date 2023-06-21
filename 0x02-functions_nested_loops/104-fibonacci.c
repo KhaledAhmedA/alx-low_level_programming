6a7b70a3637a749f6e563a34014f28e540c665b2
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-20 20:49:50 -0700

    fabi another method

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,48 +1,61 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * calclength - outer function to calc number length
+ * @num: argument of calc function
+ * Return: numbers
+*/
+int calclength(int num)
+{
+	int l = 0;
+
+	if (!l)
+		return (l);
+	while (num)
+	{
+		num = num / 10;
+		l += 1;
+	}
+	return (l);
+}
+/**
+ * main -Entry point
  * Return: Always 0 (Sucess)
 */
 int main(void)
 {
-	int i, log1, log2;
-	long int num1, num2, box1, box2, ln1, ln2;
+	int i, init;
+	unsigned long num1, num2, box1, box2, div, ln1, ln2;
 
 	num1 = 1;
 	num2 = 2;
-	log1 = 0;
-	log2 = 0;
-	for (i = 1; i <= 98; i++)
+	div = 100000000;
+	ln1 = 0;
+	ln2 = 0;
+	box2 = 0;
+
+	for (i = 1; 1 <= 98; i++)
 	{
-		if (log1)
+		if (ln1 > 0)
+			printf("%lu", ln1);
+		init = calclength(div) - 1 - calclength(num1);
+		while (ln1  > 0; init > 0)
 		{
-			box1 = num1 + num2;
-			printf("%ld, ", box1);
-			num1 = num2;
-			num2 = box1;
+			printf("%d", 0);
+			init--;
 		}
+		printf("%lu", num1);
+
+		box1 = (num1 + num2) % div;
+		box2 = ln1 + ln2 + (num1 + num2) / div;
+		num1 = num2;
+		ln1 = ln2;
+		num2 = box1;
+		ln2 = box2;
+
+		if (i != 98)
+			printf(", ");
 		else
-		{
-			if (log2)
-			{
-				ln1 = num1 % 1000000000;
-				ln2 = num2 % 1000000000;
-				num1 = num1 / 1000000000;
-				num2 = num2 / 1000000000;
-				log2 = 0;
-			}
-			box2 = (ln1 + ln2);
-			box1 = num1 + num2 + (box2 / 1000000000);
-			printf(", %ld", box1);
-			printf("%ld", box2 % 1000000000);
-			num1 = num2;
-			ln1 = ln2;
-			num2 = box1;
-			ln2 = (box2 % 1000000000);
-		}
-		if (((num1 + num2) < 0) && log1 == 1)
-			log1 = 0;
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
