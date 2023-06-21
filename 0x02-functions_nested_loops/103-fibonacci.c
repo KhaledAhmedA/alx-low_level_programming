a0eaae432d4e9733aa7094f4fec472064a5d8205
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-20 19:16:00 -0700

    fiboancci 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	long int total, num1, num2, box;
+
+	num1 = 0;
+	num2 = 1;
+
+	while (true)
+	{
+		box = num1 + num2;
+		if (box > 4000000)
+			break;
+		if ((box % 2) == 0)
+			total += box;
+		num1 = num2;
+		num2 = box;
+	}
+	printf("%ld\n", total);
+	return (0);
+}
