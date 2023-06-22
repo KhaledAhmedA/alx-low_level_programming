aa729309486a25851f3a6d8fda8d336ac00d3974
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-22 12:16:22 -0700

    print integers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - function numbers followed by newline
+ * Return: Always 0 (Sucess)
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
