489cdd038048c4a013112e4584b33794a8b53c9e
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-22 12:06:03 -0700

    multiplay two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function return multiplay of two numbers
+ * @a: first oprand
+ * @b: second operand
+ * Return: Always 0 (Sucess)
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
