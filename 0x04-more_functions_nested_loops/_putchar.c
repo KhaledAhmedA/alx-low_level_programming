10a8f73a708dc249923e801f9f7b40c7d12b94b7
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-22 11:48:07 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar - function to print to std output
+ * @str: the parameter of function carry variable to operations
+ * Return: Always 0 (Sucess)
+*/
+int _putchar(char str)
+{
+	return (write(1, &str, 1));
+}
