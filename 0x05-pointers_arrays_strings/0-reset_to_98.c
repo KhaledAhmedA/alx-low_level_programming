f874d215d69b0b63cb3c8f80f317b54946f94fa5
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-26 12:48:25 -0700

    reset tp 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function reset value to 98 with pointer
+ * @n: argument pointer of integer
+ * Return: Always 0 (Sucess)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+README File for Pointers Arrays Strings project tasks
