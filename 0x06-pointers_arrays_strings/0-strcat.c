a2603553e1333089181cf72020d531510f0fab6e
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-29 16:08:42 -0700

    test

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,10 @@ char *_strcat(char *dest, char *src)
 	int dcount, scount;
 
 	dcount = 0;
-	scount = 0;
 
-	while (*(dest + dcount) != '\0')
+	while (dest[dcount])
 		dcount++;
-	while (scount >= 0)
-	{
-		*(dest + dcount) = *(src + scount)
-			if (*(src + scount) == '\0')
-				break;
-		dcount++;
-		scount++;
-	}
+	for (scount = 0; src[scount]; scount++)
+		dest[dcount++] = src[scount];
 	return (dest);
 }
