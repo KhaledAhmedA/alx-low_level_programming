4620610c38a721f712447bb95ec2d19cc6b1a626
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-29 17:53:59 -0700

    rev arr

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,16 +3,16 @@
  * reverse_array - a function return reversed array
  * @a: given array
  * @n: size of array
- * Return: Always 0 (Sucess)
+ * Return: not return value
 */
 void reverse_array(int *a, int n)
 {
 	int i, k, box;
 
-	for (i = 0, j = (n - 1); i < j; i++, j--)
+	for (i = 0, k = (n - 1); i < k; i++, k--)
 	{
 		box = a[i];
-		a[i] = a[j];
-		a[j] = box;
+		a[i] = a[k];
+		a[k] = box;
 	}
 }
