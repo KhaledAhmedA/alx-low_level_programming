ad9c926ad162ce94a19f4e332989bc3ed383b3e4
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-29 19:39:45 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - a function exchange pattern
+ * @ch: function argument
+ * Return: p exchanged value
+*/
+char *rot13(char *ch)
+{
+	int i;
+	char rot[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char val[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *p = ch;
+
+	while (*ch)
+	{
+		for (i = 0, i < 52; i++)
+		{
+			if (*ch == rot[i])
+			{
+				*ch = val[i];
+				break;
+			}
+		}
+		ch++;
+	}
+	return (p);
+}
