c54820c083410d0c6c0551f852d05be9836b6a6d
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-06-29 17:46:37 -0700

    reversed array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - a funvtion return reversed array
+ * @a: given array
+ * @n: size of array
+ * Return: Always 0 (Sucess)
+*/
+void reverse_array(int *a, int n)
+{
+	int i, k, box;
+
+	for (i = 0, j = (n - 1); i < j; i++, j--)
+	{
+		box = a[i];
+		a[i] = a[j];
+		a[j] = box;
+	}
+}
