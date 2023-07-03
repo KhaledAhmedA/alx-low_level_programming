e8b4319c630d44623fc34efe13f7b83d57179b1b
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-07-03 11:03:57 -0700

    memcpy2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int counter;
 
 	for (counter = 0; counter < n; counter++)
-		*(dest + i) = *(src + i);
+		*(dest + counter) = *(src + counter);
 
 	return (dest);
 }
