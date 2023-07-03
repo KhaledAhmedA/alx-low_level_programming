d545309b81879d9cd744c14d2f1756bb515417e5
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-07-03 16:34:38 -0700

    print diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagsums - a function sums diagonally from two daim array
+ * @a: array
+ * @size: array length
+ * Return: not return value
+*/
+void print_diagsums(int *a, int size)
+{
+	int i, d1, d2;
+
+	d1 = 0;
+	d2 = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+			d1 += a[i];
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+			d2 += a[i];
+	}
+	printf("%d", d1);
+	printf("%d\n", d2);
+}
