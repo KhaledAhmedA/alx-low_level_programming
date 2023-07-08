4b19dcfe6c06bafc6b727fb4033a5ef3130e774f
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-07-07 17:30:35 -0700

    sum argus

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function return sum of argus nums
+ * @argc: arguments length
+ * @argv: arguments value
+ * Return: 0 (Sucess)
+*/
+int main(int argc, char **argv)
+{
+	int result;
+	char *arv;
+
+	while (--argc)
+	{
+		for (arv = argv[argc]; *arv; arv++)
+			if (*arv > '0' || *arv < '9')
+				return (printf("Error\n"), 1);
+		result += atoi(argv[argc]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
+
