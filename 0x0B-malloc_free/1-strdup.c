8298329ae8e82efd7497ee4abfa867b0c1434c74
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-07-10 14:02:57 -0700

    copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strdup - function return given string as a copy
+ * @str: given string
+ * Return: NULL in case of null or string
+*/
+
+char *_strdup(char *str)
+{
+	char *cpstr;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	cpstr = (char *) malloc((i + 1) * sizeof(char));
+
+	if (cpstr == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		str[j] = cpstr[j];
+
+	return (cpstr);
+}
