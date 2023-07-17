25f197c56b66a8e4c4b4092364298ec6bc0fda67
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-07-17 15:34:34 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - is a struct type
+ * @name: string or pointer to char
+ * @age: float
+ * @owner: pointer to char
+*/
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
+
+#endif
