0e53cc3382105be28a449d6ba882d6562eff4918
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-07-17 13:11:43 -0700

    current program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point print current path of compiled prog
+ *
+ * Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	printf("%s\n",__FILE__);
+
+	return (0);
+}
