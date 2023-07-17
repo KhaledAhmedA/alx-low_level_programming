4c09c90e694ec207a9a2480527381632497f01e2
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-07-17 15:44:33 -0700

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - function init dog struct type variables
+ * @d: struct type
+ * @name: name in the struct
+ * @age: var of struct
+ * @owner: char var in struct
+ * Return: not return any value
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	while (d)
+	{
+		*(d).name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
