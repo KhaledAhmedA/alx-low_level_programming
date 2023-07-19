397da2030117ed8237ce45b888b8a091bccba588
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-07-19 08:19:38 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
-	{
-		if (size <= 0)
-			return (-1);
-
-		for (i = 0; i < size; i++)
+	i = 0;
+	if (array && cmp && size)
+		while (i < size)
+		{
 			if (cmp(array[i]))
-				return (1);
-	}
+				return (i);
+			i++;
+		}
 	return (-1);
 }
