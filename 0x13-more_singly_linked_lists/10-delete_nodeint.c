7eb02fa57481f1e5f2dca513ac273077da11b239
Khaled Ahmed <khaled.ahmed.alkharashi@gmail.com>
2023-08-01 16:31:43 -0700

    delete indexed node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	node = *head;
 	while (node)
 	{
-		if (i == node)
+		if (i == index)
 		{
 			prevNode->next = node->next;
 			free(node);
